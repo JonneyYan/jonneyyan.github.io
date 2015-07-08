ecedfe0c17cb69fee3b69844cf960e2493d69ce4
jonneyyan <jonney0828@gmail.com>
2015-07-08 16:46:54 +0800

    上传六龙御天

---
+++ src/js/common.js
@@ -0,0 +1,44 @@
+ /***设备选择提示***/
+$(function(){
+	var bd = $(document.body);
+	window.addEventListener('onorientationchange' in window ? 'orientationchange' : 'resize', _orientationchange, false);
+	function _orientationchange() {
+		scrollTo(0, 1);
+		switch(window.orientation){
+			case 0:		//横屏			
+				break;
+			case 180:	//横屏
+				break;
+			case -90: 	//竖屏
+				alert("请竖屏查看页面，效果更佳。");
+				break;
+			case 90: 	//竖屏
+				alert("请竖屏查看页面，效果更佳。");
+				break;
+		}
+	}
+	$(window).on('load',_orientationchange);
+});
+
+/***添加视察效果***/
+window.addEventListener("deviceorientation", motion);
+
+/******背景移动******/
+function motion(event){
+	var x = Math.sin(event.gamma * Math.PI / 180) * 20;
+	var y = Math.sin(event.beta * Math.PI / 180) * 20;
+	// beginX = beginX + x;
+		 
+	// var picW = $(".P1_0").width();
+	// var screenW = $(".page1").width();
+	// if(beginX > 0)
+	// 	beginX = 0;
+	// else if(beginX < screenW - picW)
+	// 	beginX = screenW - picW;
+		
+	// $(".P1_0").css("left", beginX + "px");
+	$(".shift").each(function() {
+		
+	};)
+	 
+}
